6c3b87fe03bcdd95ba3a83966c5ee7779865b81f
supercoolcoder88 <supercoolcoder88@gmail.com>
2026-01-21 22:27:42 +1100

    WIP: UI creation

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
@@ -33,18 +34,25 @@ func main() {
 
 	grid := tview.NewGrid().
 		SetRows(0).
-		SetColumns(3, 7)
+		SetColumns(35, 0)
 
 	// Filter area
 	fieldFilterSidebar := tview.NewForm()
+	fieldFilterSidebar.SetItemPadding(1).SetBorder(true).SetTitle(" Filters ")
 
 	activeFilters := make(map[string]string)
 	for _, field := range logs.CommonFields {
-		fieldFilterSidebar.AddInputField(field, "", 30, nil, func(text string) {
+		fieldFilterSidebar.AddInputField(field, "", 0, nil, func(text string) {
 			activeFilters[field] = text
 		})
 	}
 
+	// Search bar component
+	searchbar := tview.NewInputField().
+		SetLabel("Search: ").
+		SetFieldBackgroundColor(tcell.ColorDarkSlateGrey)
+
+	// Search button
 	searchButton := tview.NewButton("Search")
 
 	searchButton.SetSelectedFunc(func() {
@@ -63,11 +71,34 @@ func main() {
 		}
 	})
 
+	// Search row
+	searchRow := tview.NewFlex().
+		AddItem(searchbar, 0, 1, true).
+		AddItem(searchButton, 12, 0, false)
+
+	// Draw items
+	// Create a placeholder for where the logs will go
+	logDisplay := tview.NewTextView().
+		SetDynamicColors(true).
+		SetRegions(true).
+		SetWordWrap(true)
+	logDisplay.SetBorder(true).SetTitle(" Log Output ")
+
+	// searchrow on top, logs on bot
+	rightSide := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(searchRow, 3, 0, false).
+		AddItem(logDisplay, 0, 1, false)
+
+	// Draw grid
+	grid.Clear()
+	grid.SetRows(0).SetColumns(35, 0)
 	grid.AddItem(fieldFilterSidebar, 0, 0, 1, 1, 0, 0, true)
+	grid.AddItem(rightSide, 0, 1, 1, 1, 0, 0, false)
 
 	// Log display area
 	// textArea := tview.NewTextView()
-	if err := app.SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
+	if err := app.EnableMouse(true).SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
 		panic(err)
 	}
 }
